9dc70a2d9cb8eb810a8fde985deffa9351584232
Anatoliy <Dubrovskiy.ru@mail.ru>
2022-01-18 13:24:53 +0300

    les_05

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,88 @@
-import { renderSearchFormBlock } from './search-form.js';
-import { renderSearchStubBlock } from './search-results.js';
-import { renderUserBlock } from './user.js';
-import { renderToast } from './lib.js';
+// Реализовать абстрактный класс MyGraphicsPrimitive2D у которого есть следующие свойства:
+//   прямоугольная область, описывающая примитив; метод - переместить примитив на заданное смещение;
+// От него дожен наследоваться абстрактный класс MyAreaPrimitive2D, у которого есть свойство площадь.
+//   От него должны наследоваться класс MyCircle, у него есть свойства: центр окружности и ее радиус,
+//   а также должен наследоваться класс MyRectangle с свойствами: ширина и высота, левая верхняя граница,
+//   правая нижняя граница
 
-const user = {
-  userName: 'Wade Warren',
-  avatarUrl: '/img/avatar.png'
-};
+export interface ICoordinates {
+  x: number,
+  y: number
+}
+export interface IFigureCircle {
+  radius: number;
+  circleCenter: ICoordinates;
+}
+export interface IFigureRectangle {
+  center: ICoordinates;
+  width: number;
+  height: number
+}
 
-// //заводим данные в localStorage
-function setItemUser(key, value) {
-  try {
-    return window.localStorage.setItem(key, value)
-  } catch (e) {
-    console.log(e)
+export abstract class MyGraphicsPrimitive2D {
+  private _coordinates: ICoordinates;
+  constructor(x: number, y: number) {
+    this._coordinates = {x, y}
   }
-};
-function setItemFavorites(key, value) {
-  try {
-    return window.localStorage.setItem(key, value)
-  } catch (e) {
-    console.log(e)
+  getCenter(): Readonly<ICoordinates> {
+    return this._coordinates
   }
-};
-setItemUser('user', JSON.stringify(user));
-setItemFavorites('favoritesAmount', '1');
-
-// //получаем данные из localStorage
-function getItemUser(key) {
-  try {
-    return window.localStorage.getItem(key);
-  } catch (e) {
-    console.log(e)
+  movePrimitive(x: number, y: number) {
+    this._coordinates.x = x;
+    this._coordinates.y = y;
   }
-};
+}
 
-function getUserData(key) : string {
-  try {
-    const json = getItemUser(key)
+export abstract class MyAreaPrimitive2D extends MyGraphicsPrimitive2D {
+  abstract x: number;
+  abstract y: number;
+  abstract getArea(): void;
+}
 
-    return JSON.parse(json)
-  } catch (e) {
-    console.error(e)
+export class MyCircle extends MyAreaPrimitive2D {
+  private _circleCenter: IFigureCircle;
+  constructor(public x: number, public y: number, radius) {
+    super(x, y);
+    this._circleCenter = {
+      circleCenter: this.circleCenter,
+      radius
+    }
   }
-};
-
-function getFavoritesAmount(key) : number {
-  try {
-    return parseInt(window.localStorage.getItem(key));
-  } catch (e) {
-    console.log(e)
+  getCircleCenter(): Readonly<ICoordinates> {
+    return this.circleCenter
   }
-};
+  getRadius(): Readonly<number> {
+    return this._circleCenter.radius;
+  }
+  getArea(): number {
+    return Math.PI * (this.radius * 2)
+  }
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock(getUserData('user.userName'), getUserData('user.avatarUrl'), getFavoritesAmount('favoritesAmount'));
-  renderSearchFormBlock('','');
-  renderSearchStubBlock();
-  renderToast(
-    {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
-    {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
-  );
-});
+export class MyRectangle extends MyAreaPrimitive2D {
+  private _rectangle: IFigureRectangle;
+  constructor(public x: number, public y: number, public width: number, public height: number) {
+    super(x, y);
+    this._rectangle = {center: this.center, width, height}
+  }
+  getArea(): number {
+    return this._rectangle.width * this._rectangle.height
+  }
+  getWidth(): number {
+    return this._rectangle.width
+  }
+  getHeight(): number {
+    return this._rectangle.height
+  }
+  getTopLeftBorder(): Readonly<ICoordinates> {
+    return this.center
+  }
+  getRightBottomBorder(): Readonly<ICoordinates> {
+    return {
+      x: this.center.x + this._rectangle.width,
+      y: this.center.y + this._rectangle.height
+    }
+  }
+}
+
+
